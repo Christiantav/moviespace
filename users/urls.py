a898a9754cd4ed03ead70ed968d8ec0d856558fe
unknown <jaseystudios@gmail.com>
2020-03-01 16:15:47 -0500

    comparison commit 2

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
 from .api import UserViewSet
 router = routers.DefaultRouter()
-router.register('api/user',UserViewSet,'user')
+router.register('api/users',UserViewSet,'users')
 urlpatterns = router.urls
