efe3094007d71e151762ac9cc02f6c42c4cce624
Christian <chrisadrianotav@gmail.com>
2020-04-02 20:46:50 -0400

    change router.register link to api/user

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
 from .api import UserViewSet
 router = routers.DefaultRouter()
-router.register('api/users',UserViewSet,'users')
+router.register('api/user',UserViewSet,'user')
 urlpatterns = router.urls
